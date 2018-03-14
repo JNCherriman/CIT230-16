78030f2793207d46815c6f7d0e1a51b7b8e044f3
Jeremy Cherriman <32113470+JNCherriman@users.noreply.github.com>
2018-03-14 00:41:41 -0400

    Usability tweaks

--- assignments/lesson-9/scripts/townsummaries.js
+++ assignments/lesson-9/scripts/townsummaries.js
@@ -14,27 +14,42 @@ request.onload = function() {
     for (var i = 0; i < cities.length; i++) {
         if (cities[i].name == 'Placerton')
             i++;
+        var myA = document.createElement('a');
+        myA.setAttribute('href',cities[i].name.toLowerCase() + '-9.html')
         var mySection = document.createElement('section');
-        mySection.setAttribute('class','bodyStandard');
+        mySection.setAttribute('class','bodyGenerated');
         var myH3 = document.createElement('h3');
+        var mySpan1 = document.createElement('span');
+        mySpan1.setAttribute('class','cityInfo');
         var myPara1 = document.createElement('p');
         var myPara2 = document.createElement('p');
         var myPara3 = document.createElement('p');
         var myPara4 = document.createElement('p');
+        var mySpan2 = document.createElement('span');
+        mySpan2.setAttribute('class','imgWrapper');
+        var myImg1 = document.createElement('img');
 
         myH3.textContent = cities[i].name;
         myPara1.textContent = 'Town Motto: ' + cities[i].motto;
         myPara2.textContent = 'Year Founded: ' + cities[i].yearFounded;
         myPara3.textContent = 'Population: ' + cities[i].currentPopulation;
         myPara4.textContent = 'Annual Rainfall: ' + cities[i].averageRainfall;
+        myImg1.setAttribute('src','../../images/' + cities[i].name.toLowerCase() +'hmpg.jpg');
+        myImg1.setAttribute('alt','Come vist' + cities[i].name);
 
-        mySection.appendChild(myH3);
-        mySection.appendChild(myPara1);
-        mySection.appendChild(myPara2);
-        mySection.appendChild(myPara3);
-        mySection.appendChild(myPara4);
+        mySpan1.appendChild(myH3);
+        mySpan1.appendChild(myPara1);
+        mySpan1.appendChild(myPara2);
+        mySpan1.appendChild(myPara3);
+        mySpan1.appendChild(myPara4);
+        mySpan2.appendChild(myImg1);
 
-        output.appendChild(mySection);
+        mySection.appendChild(mySpan1);
+        mySection.appendChild(mySpan2);
+
+        myA.appendChild(mySection);
+
+        output.appendChild(myA);
     }
 }
 
