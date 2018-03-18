35df5a297200007615336a34b85c9e6397161cc4
Jeremy Cherriman <32113470+JNCherriman@users.noreply.github.com>
2018-03-18 15:29:27 -0400

    Start of final weather site

---
+++ assignments/weathersite/scripts/townsummaries.js
@@ -0,0 +1,57 @@
+var output = document.querySelector('#townsummaries');
+
+var requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+request.onload = function() {
+    var jsondata = request.response;
+    var cities = jsondata['towns'];
+
+    for (var i = 0; i < cities.length; i++) {
+        if (cities[i].name == 'Placerton')
+            i++;
+        var myA = document.createElement('a');
+        myA.setAttribute('href',cities[i].name.toLowerCase() + '-9.html')
+        var mySection = document.createElement('section');
+        mySection.setAttribute('class','bodyGenerated');
+        var myH3 = document.createElement('h3');
+        var mySpan1 = document.createElement('span');
+        mySpan1.setAttribute('class','cityInfo');
+        var myPara1 = document.createElement('p');
+        var myPara2 = document.createElement('p');
+        var myPara3 = document.createElement('p');
+        var myPara4 = document.createElement('p');
+        var mySpan2 = document.createElement('span');
+        mySpan2.setAttribute('class','imgWrapper');
+        var myImg1 = document.createElement('img');
+
+        myH3.textContent = cities[i].name;
+        myPara1.textContent = 'Town Motto: ' + cities[i].motto;
+        myPara2.textContent = 'Year Founded: ' + cities[i].yearFounded;
+        myPara3.textContent = 'Population: ' + cities[i].currentPopulation;
+        myPara4.textContent = 'Annual Rainfall: ' + cities[i].averageRainfall;
+        myImg1.setAttribute('src','../../images/' + cities[i].name.toLowerCase() +'hmpg.jpg');
+        myImg1.setAttribute('alt','Come vist' + cities[i].name);
+
+        mySpan1.appendChild(myH3);
+        mySpan1.appendChild(myPara1);
+        mySpan1.appendChild(myPara2);
+        mySpan1.appendChild(myPara3);
+        mySpan1.appendChild(myPara4);
+        mySpan2.appendChild(myImg1);
+
+        mySection.appendChild(mySpan1);
+        mySection.appendChild(mySpan2);
+
+        myA.appendChild(mySection);
+
+        output.appendChild(myA);
+    }
+}
+
+//var towninfo = request.response;
+// document.getElementById('townsummaries').innerHTML = output;
---
+++ assignments/weathersite/scripts/wu-weather.js
@@ -0,0 +1,17 @@
+var requestURL = 'https://api.wunderground.com/api/62c9f84e4efe3237/conditions/q/MN/Franklin.json';
+
+var weatherObject = new XMLHttpRequest();
+weatherObject.open('GET', requestURL, true);
+weatherObject.send();
+
+weatherObject.onload = function() {
+    var weatherInfo = JSON.parse(weatherObject.responseText);
+    document.getElementById('currentTemp').innerHTML = weatherInfo.current_observation.temp_f;
+    document.getElementById('feelsLike').innerHTML = weatherInfo.current_observation.feelslike_f;
+    document.getElementById('weather').innerHTML = weatherInfo.current_observation.weather;
+    document.getElementById('w_icon').src = weatherInfo.current_observation.icon_url.replace("http", "https");
+    document.getElementById('precipAmt').innerHTML = weatherInfo.current_observation.precip_today_string;
+    document.getElementById('windSpeed').innerHTML = weatherInfo.current_observation.wind_mph;
+    document.getElementById('windDir').innerHTML = weatherInfo.current_observation.wind_dir;
+    document.getElementById('windchill').innerHTML = weatherInfo.current_observation.windchill_f;
+}
