027c2cc6ad6473db04799049f58a94bf2eaf3d35
Jeremy Cherriman <32113470+JNCherriman@users.noreply.github.com>
2018-03-19 20:15:52 -0400

    Gallery popup successful!

--- assignments/weathersite/scripts/imgpop.js
+++ assignments/weathersite/scripts/imgpop.js
@@ -1,14 +1,14 @@
-(function () {
+$(function () {
     "use strict";
 
-    (".gallery figure img").click(function () {
-        var src = (this).attr("src");
-        (".show").fadeIn();
-        (".img-show img").attr("src", src);
+    $(".gallery figure img").click(function () {
+        var $src = $(this).attr("src");
+        $(".show").fadeIn();
+        $(".img-show img").attr("src", $src);
     });
 
-    ("span, .overlay").click(function () {
-        (".show").fadeOut();
+    $("span, .overlay").click(function () {
+        $(".show").fadeOut();
     });
 
 });
