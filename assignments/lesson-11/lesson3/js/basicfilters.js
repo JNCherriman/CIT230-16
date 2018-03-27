e4691276b9e1c659775b99895428fc688e3e3b8a
Jeremy Cherriman <32113470+JNCherriman@users.noreply.github.com>
2018-03-27 07:03:14 -0400

    Full refresh of lesson 11

---
+++ assignments/lesson-11/lesson3/js/basicfilters.js
@@ -0,0 +1,6 @@
+    $(function(){
+    // All jQuery code goes here
+
+    $('li:eq(3)').css('background-color', 'rgb(248, 95, 95)');
+
+})
---
+++ assignments/lesson-11/lesson3/js/scripts.js
@@ -0,0 +1,24 @@
+$(function () {
+    // All jQuery code goes here
+    // $('#alcoveLogo').css('opacity', 0.4);
+
+    // $('#alcoveLogo').next().css('background-color', 'rgba(255, 0, 0, 0.2');
+
+    $('.header').children().css('background-color', 'rgba(255, 0, 0, 0.2');
+
+    $('.servicesBtn').closest('div').css('background-color', 'rgba(255, 0, 0, 0.2');
+
+    $('.navigation > li:eq(2)').parent().css('opacity', 0.3);
+
+    $('.servicesBtn').css('color', '#FFF');
+
+    $('button').css('border', '1px solid red');
+
+    $('h2').css('border', '1px solid red');
+
+    $('input[name$="Val"]').css('border', '1px solid red');
+
+    // $('.box1').nextAll().css('background-color', 'rgba(255, 0, 0, 0.2');
+
+
+})
