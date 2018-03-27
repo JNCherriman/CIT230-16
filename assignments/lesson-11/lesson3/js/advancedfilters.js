bfffdfd1cf45e131831c434a459e9bee5220145e
Jeremy Cherriman <32113470+JNCherriman@users.noreply.github.com>
2018-03-27 07:02:25 -0400

    Attempting full refresh of lesson 11

--- assignments/lesson-11/lesson3/js/advancedfilters.js
+++
@@ -1,5 +0,0 @@
-$(function(){
-    // All jQuery code goes here
-    $('li:nth-child(3)').css('background-color', 'rgb(248, 95, 95)');
-
-})
--- assignments/lesson-11/lesson3/js/basicfilters.js
+++
@@ -1,6 +0,0 @@
-    $(function(){
-    // All jQuery code goes here
-
-    $('li:eq(3)').css('background-color', 'rgb(248, 95, 95)');
-
-})
